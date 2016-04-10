4ff64a4696564f31f1817eeb987cf9b037fed985
384401056 <384401056@qq.com>
2016-04-10 20:38:30 +0800

    AngularJS

---
+++ AngularJS/定义模块/js/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/4/9.
+ */
+//angular.module("myApp",[]).run(function($rootScope){
+//   $rootScope.name = "World";
+//});
+
+angular.module("myApp",[]).controller("myControl",function($scope){
+   $scope.name = "World";
+});
