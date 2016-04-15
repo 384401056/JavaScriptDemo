d2b469ca9424dea77f7dca3c4efa93f0e23a797a
Gaoyanbin <384401056@qq.com>
2016-04-15 09:39:45 +0800

    购物车项目

---
+++ AngularJS/WebSocket/js/app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2016/4/9.
+ */
+
+
+var app = angular.module("websocketApp",[]);
+
+app.controller("firstController",function($scope){
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
