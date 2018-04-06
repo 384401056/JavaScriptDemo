7ff7c68db856ebec076f0a3ca3be1a67672adecd
384401056 <384401056@qq.com>
2018-04-06 12:17:04 +0800

    gethttp.component.ts

--- AngularJS4x/Demo01/src/app/component/gethttp/gethttp.component.ts
+++ AngularJS4x/Demo01/src/app/component/gethttp/gethttp.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Http, Jsonp } from '@angular/http'; //引入http
+import { Http, Jsonp, Headers, RequestOptions, URLSearchParams } from '@angular/http'; //引入http
 
 @Component({
   selector: 'app-gethttp',
@@ -8,14 +8,17 @@ import { Http, Jsonp } from '@angular/http'; //引入http
 })
 export class GethttpComponent implements OnInit {
 
-  private inputValue: string;
-  private newsList: any[];
+  private url1: string;
+  private url2: string;
+  private newsList1: any[];
+  private newsList2: any[];
+  private username: string;
+  private age: number;
 
   //注入http和jsonp
   constructor(private http: Http, private jsonp: Jsonp) {
-
-    this.inputValue = 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1';
-
+    this.url1 = 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1';
+    this.url2 = 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1&callback=JSONP_CALLBACK';
   }
 
   ngOnInit() {
@@ -23,12 +26,45 @@ export class GethttpComponent implements OnInit {
   }
 
   getHttpRequest() {
-    this.http.get(this.inputValue).subscribe(data => {
+    this.http.get(this.url1).subscribe(data => {
       console.log(data['_body']);
-      this.newsList = JSON.parse(data['_body']).result;
+      this.newsList1 = JSON.parse(data['_body']).result;
     }, err => {
       console.log(err);
     });
   }
 
+  getJsonpRequest() {
+    this.jsonp.get(this.url2).subscribe(data => {
+      console.log(data['_body']);
+      this.newsList2 = data['_body'].result; //jsonp请求返回的已经是json对象了。
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  postRequest(param1, param2) {
+    let name: string = param1;
+    let age: number = parseInt(param2, 10);
+
+    //当请求头是这样的，要用URLSearchParams来传递参数。
+    let headers: Headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' });
+    let params: URLSearchParams = new URLSearchParams();
+    params.append('username', JSON.stringify(name));
+    params.append('age', JSON.stringify(age));
+    
+    //当请求头是 application/json时，要用如下的参数。同时服务器端也要做相应的改变。
+    // let headers: Headers = new Headers({ 'Content-Type': 'application/json;charset=UTF-8' });
+    // let params = {"username":name, "age":age};
+    
+    let options: RequestOptions = new RequestOptions({ headers: headers });
+    this.http.post(
+      'http://127.0.0.1:5764/index', params, options).subscribe(data => {
+      console.log(data);
+    }, err => {
+      console.log(err);
+    });
+
+
+  }
 }
